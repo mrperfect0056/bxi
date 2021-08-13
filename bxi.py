b7e0c067ac6cb255a6443aa1ef3414de4b885d24
Binyamin Binni <53377458+Binyamin-binni@users.noreply.github.com>
2021-08-13 17:50:21 +0500

    Update bxi.py

--- bxi.py
+++ bxi.py
@@ -7,7 +7,7 @@ except ImportError:
     os.system("pip install requests")
 import requests
 
-v="2.1"
+v="2.2"
 arch=subprocess.check_output("uname -om", shell=True).decode()
 os.system("clear")
 print(" Checking Update . . .")
